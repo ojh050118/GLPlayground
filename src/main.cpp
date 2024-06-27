b48d966da1bfddfdbdf01179450369c68f45128d
Junho <51792037+ojh050118@users.noreply.github.com>
2024-06-27 22:57:29 +0900

    Hello Shader

--- src/main.cpp
+++ src/main.cpp
@@ -15,27 +15,37 @@ void processInput(GLFWwindow* window)
 
 const char* vertexShaderSource = "#version 330 core\n"
                           "layout (location = 0) in vec3 vPos;\n"
+                          "layout (location = 1) in vec3 vCol;\n"
+                          "\n"
+                          "out vec3 vColor;\n"
                           "\n"
                           "void main()\n"
                           "{\n"
                           "    gl_Position = vec4(0.8f * vPos.x, 0.8f * vPos.y, 0.8f * vPos.z, 1.0);\n"
+                          "    vColor = vCol;\n"
                           "}";
 
 const char* fragmentShaderSource = "#version 330 core\n"
-                            "out vec4 FragColor;\n"
-                            "\n"
-                            "void main()\n"
-                            "{\n"
-                            "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                            "} ";
-
-const char* fragmentShaderSource2 = "#version 330 core\n"
+                                   "in vec3 vColor;\n"
                                    "out vec4 FragColor;\n"
+                                   "\n"
+                                   "uniform float time;\n"
                                    "\n"
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = vec4(0.2f, 0.5f, 1.0f, 1.0f);\n"
+                                   "    FragColor = vec4(vColor, 1.0f);\n"
                                    "} ";
+
+const char* fragmentShaderSource2 = "#version 330 core\n"
+                                    "in vec3 vColor;\n"
+                                    "out vec4 FragColor;\n"
+                                    "\n"
+                                    "uniform float time;\n"
+                                    "\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "    FragColor = vec4(vColor.x * abs(tan(time)), vColor.y * abs(tan(time * 2)), vColor.z * abs(tan(time * 3)), 1.0f);\n"
+                                    "} ";
 
 void addShader(GLuint program, const char* source, GLenum type)
 {
@@ -87,15 +97,15 @@ int main() {
 
     // prepare to rendering
     GLfloat vertices[] = {
-            -0.5f, 0.25f, 1.0f,
-            -1.0f, -0.25f, 1.0f,
-            0.0f, -0.25f, 1.0f
+            -0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f,
+            -1.0f, -0.5f, 1.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f
     };
 
     GLfloat vertices2[] = {
-            1.0f, 0.25f, 1.0f,
-            0.0f, 0.25f, 1.0f,
-            0.5f, -0.25f, 1.0f
+            1.0f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.5f, 1.0f, 0.0f, 1.0f, 0.0f,
+            0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f
     };
 
     GLuint VAO[2], VBO[2], shaderProgram, shaderProgram2;
@@ -107,15 +117,19 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (void*)(sizeof(GLfloat) * 3));
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     glBindVertexArray(VAO[1]);
     glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (void*)(sizeof(GLfloat) * 3));
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     GLint success;
     GLchar infoLog[1024];
@@ -166,10 +180,14 @@ int main() {
         glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        glUniform1f(glGetUniformLocation(shaderProgram, "time"), (float)glfwGetTime());
+
         glUseProgram(shaderProgram2);
 
         glBindVertexArray(VAO[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUniform1f(glGetUniformLocation(shaderProgram2, "time"), (float)glfwGetTime());
 
         glBindVertexArray(0);
         glUseProgram(0);
