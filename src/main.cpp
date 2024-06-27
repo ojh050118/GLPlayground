6c9fcf3de586f9913be46c5cd003d4f433717ada
Junho <51792037+ojh050118@users.noreply.github.com>
2024-06-27 17:24:15 +0900

    Hello Triangle: 2 Triangles, 1 VAO, 1 VBO

--- src/main.cpp
+++ src/main.cpp
@@ -79,29 +79,25 @@ int main() {
 
     // prepare to rendering
     GLfloat vertices[] = {
-            0.0f, 1.0f, 1.0f,   // Top Left
-            1.0f, 1.0f, 1.0f,   // Top Right
-            -1.0f, -1.0f, 1.0f, // Bottom Left
-            1.0f, -1.0f, 1.0f   // Bottom Right
+            // Left Triangle
+            -0.5f, 0.25f, 1.0f,
+            -1.0f, -0.25f, 1.0f,
+            0.0f, -0.25f, 1.0f,
+
+            // Right Triangle
+            0.5f, 0.25f, 1.0f,
+            0.0f, -0.25f, 1.0f,
+            1.0f, -0.25f, 1.0f
     };
 
-    GLuint indices[] { 0, 1, 2,
-                       2, 3, 1 };
-
-    GLuint VAO, VBO, EBO, shaderProgram;
+    GLuint VAO, VBO, shaderProgram;
 
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(0);
@@ -145,8 +141,7 @@ int main() {
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
 
-        // glDrawArrays(GL_TRIANGLES, 1, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glBindVertexArray(0);
         glUseProgram(0);
