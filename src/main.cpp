390e4fa8158b0af75e22624d2909c9aa84ea0a4c
Junho <51792037+ojh050118@users.noreply.github.com>
2024-10-14 14:18:23 +0900

    계속해서 움직이는 회전값

--- src/main.cpp
+++ src/main.cpp
@@ -96,6 +96,8 @@ int main() {
     float maxTriOffset = 0.5f;
     bool direction;
 
+    float rotation = 0;
+
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
@@ -132,8 +134,13 @@ int main() {
         if (abs(triOffset) > maxTriOffset)
             direction = !direction;
 
+        rotation += 0.05f;
+
+        if (rotation >= 360)
+            rotation -= 360;
+
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, 90 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::rotate(model, rotation * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
         //model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
