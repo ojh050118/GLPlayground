a2f64bcbd0beee8bc1859e2e49b2628ecdc95d76
Junho <ojh0050118@gmail.com>
2025-01-12 23:02:30 +0900

    삼각형 스케일링

--- src/main.cpp
+++ src/main.cpp
@@ -140,8 +140,9 @@ int main() {
             rotation -= 360;
 
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, rotation * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-        //model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        //model = glm::rotate(model, rotation * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 1.0f));
 
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
